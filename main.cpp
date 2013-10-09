0b533d06ba7c4122f1ae286cc20226a62875d501
Bruno Maia <queimadus@gmail.com>
2013-10-09 18:51:32 +0100

    changes

--- main.cpp
+++ main.cpp
@@ -14,25 +14,31 @@
 #include <AR/video.h>
 #include <AR/param.h>
 #include <AR/ar.h>
+#include <AR/arMulti.h>
 #include <math.h>
+
 
 //
 // Camera configuration.
 //
 #ifdef _WIN32
 char			*vconf = "Data\\WDM_camera_flipV.xml";
+char            *cparam_name = "Data\\camera_para.dat";
+char                *config_name = "Data\\marker.dat";
 #else
 char			*vconf = "v4l2src device=/dev/video1 use-fixed-fps=false ! ffmpegcolorspace ! capsfilter caps=video/x-raw-rgb,bpp=24,width=640,height=480 ! identity name=artoolkit ! fakesink";
+char           *cparam_name    = "Data/camera_para.dat";
+char            *config_name = "Data/marker.dat";
 #endif
 
 int             xsize, ysize;
 int             thresh = 100;
 int             count = 0;
 
-char           *cparam_name    = "Data/camera_para.dat";
 ARParam         cparam;
+//char                *config_name = "Data/multi/marker.dat";
+ARMultiMarkerInfoT  *config;
 
-char           *patt_name      = "Data/patt.hiro";
 int             patt_id;
 double          patt_width     = 80.0;
 double          patt_center[2] = {0.0, 0.0};
@@ -52,7 +58,7 @@ static void   init(void);
 static void   cleanup(void);
 static void   keyEvent( unsigned char key, int x, int y);
 static void   mainLoop(void);
-static void   draw( void );
+static void   draw( double trans1[3][4], double trans2[3][4], int mode );
 
 void holeMaterial() {
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
@@ -130,6 +136,28 @@ static void   keyEvent( unsigned char key, int x, int y)
         cleanup();
         exit(0);
     }
+	/*
+	 if( key == 't' ) {
+        printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
+        printf("Enter new threshold value (current = %d): ", thresh);
+        scanf("%d",&thresh); while( getchar()!='\n' );
+        printf("\n");
+        count = 0;
+    }
+	*/
+    /* turn on and off the debug mode with right mouse */
+    if( key == 'd' ) {
+        printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
+        arDebug = 1 - arDebug;
+        if( arDebug == 0 ) {
+            glClearColor( 0.0, 0.0, 0.0, 0.0 );
+            glClear(GL_COLOR_BUFFER_BIT);
+            argSwapBuffers();
+            glClear(GL_COLOR_BUFFER_BIT);
+            argSwapBuffers();
+        }
+        count = 0;
+    }
 }
 
 /* main loop */
@@ -138,7 +166,9 @@ static void mainLoop(void)
     ARUint8         *dataPtr;
     ARMarkerInfo    *marker_info;
     int             marker_num;
-    int             j, k;
+    //int             j, k;
+	double          err;
+    int             i;
 
     /* grab a vide frame */
     if( (dataPtr = (ARUint8 *)arVideoGetImage()) == NULL ) {
@@ -159,8 +189,18 @@ static void mainLoop(void)
 
     arVideoCapNext();
 
+	if( (err=arMultiGetTransMat(marker_info, marker_num, config)) < 0 ) {
+		argSwapBuffers();
+        return;
+    }
+    printf("err = %f\n", err);
+    if(err > 100.0 ) {
+        argSwapBuffers();
+        return;
+    }
+
     /* check for object visibility */
-    k = -1;
+    /*k = -1;
     for( j = 0; j < marker_num; j++ ) {
         if( patt_id == marker_info[j].id ) {
             if( k == -1 ) k = j;
@@ -171,19 +211,57 @@ static void mainLoop(void)
         argSwapBuffers();
         return;
     }
-
+	*/
     /* get the transformation between the marker and the real camera */
-    arGetTransMat(&marker_info[k], patt_center, patt_width, patt_trans);
+    /*arGetTransMat(&marker_info[k], patt_center, patt_width, patt_trans);
 
-    draw();
+    draw();*/
+
+	argDrawMode3D();
+    argDraw3dCamera( 0, 0 );
+    glClearDepth( 1.0 );
+    glClear(GL_DEPTH_BUFFER_BIT);
+	
+    for( i = 0; i < config->marker_num; i++ ) {
+        if( config->marker[i].visible >= 0 ) draw( config->trans, config->marker[i].trans, 0 );
+        else                                 draw( config->trans, config->marker[i].trans, 1 );
+    }
 
     argSwapBuffers();
 }
 
 static void init( void )
 {
+	/*
     ARParam  wparam;
 	
+   
+    if( arVideoOpen( vconf ) < 0 ) exit(0);
+   
+    if( arVideoInqSize(&xsize, &ysize) < 0 ) exit(0);
+    printf("Image size (x,y) = (%d,%d)\n", xsize, ysize);
+
+    
+    if( arParamLoad(cparam_name, 1, &wparam) < 0 ) {
+        printf("Camera parameter load error !!\n");
+        exit(0);
+    }
+    arParamChangeSize( &wparam, xsize, ysize, &cparam );
+    arInitCparam( &cparam );
+    printf("*** Camera Parameter ***\n");
+    arParamDisp( &cparam );
+
+    if( (patt_id=arLoadPatt(patt_1)) < 0 ) {
+        printf("pattern load error !!\n");
+        exit(0);
+    }
+
+    
+    argInit( &cparam, 1.0, 0, 0, 0, 0 );
+*/
+
+	 ARParam  wparam;
+
     /* open the video path */
     if( arVideoOpen( vconf ) < 0 ) exit(0);
     /* find the size of the window */
@@ -200,13 +278,17 @@ static void init( void )
     printf("*** Camera Parameter ***\n");
     arParamDisp( &cparam );
 
-    if( (patt_id=arLoadPatt(patt_name)) < 0 ) {
-        printf("pattern load error !!\n");
+    if( (config = arMultiReadConfigFile(config_name)) == NULL ) {
+        printf("config data load error !!\n");
         exit(0);
     }
 
     /* open the graphics window */
-    argInit( &cparam, 1.0, 0, 0, 0, 0 );
+    argInit( &cparam, 1.0, 0, 2, 1, 0 );
+    arFittingMode   = AR_FITTING_TO_IDEAL;
+    arImageProcMode = AR_IMAGE_PROC_IN_HALF;
+    argDrawMode     = AR_DRAW_BY_TEXTURE_MAPPING;
+    argTexmapMode   = AR_DRAW_TEXTURE_HALF_IMAGE;
 }
 
 /* cleanup function called when program exits */
@@ -217,7 +299,7 @@ static void cleanup(void)
     argCleanup();
 }
 
-static void draw( void )
+static void draw( double trans1[3][4], double trans2[3][4], int mode )
 {
     argDrawMode3D();
     argDraw3dCamera( 0, 0 );
@@ -226,10 +308,17 @@ static void draw( void )
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
     
-    /* load the camera transformation matrix */
+	glMatrixMode(GL_MODELVIEW);
+    argConvGlpara(trans1, gl_para);
+    glLoadMatrixd( gl_para );
+    argConvGlpara(trans2, gl_para);
+    glMultMatrixd( gl_para );
+
+    /* load the camera transformation matrix 
     argConvGlpara(patt_trans, gl_para);
     glMatrixMode(GL_MODELVIEW);
     glLoadMatrixd( gl_para );
+*/
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
