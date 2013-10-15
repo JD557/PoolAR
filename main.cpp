3062e6ca83ed2109560657c6feea71810ada1bff
João Costa <jdpc557@gmail.com>
2013-10-15 22:03:20 +0100

    Improved Computer Vision Processing (Needs calibration on white light)

--- main.cpp
+++ main.cpp
@@ -117,6 +117,150 @@ static void   keyEvent( unsigned char key, int x, int y)
     }
 }
 
+void alphaErode(ARUint8 *data, int w, int h) {
+	int kernel[5][5];
+	kernel[0][0]=0;kernel[0][1]=1;kernel[0][2]=1;kernel[0][3]=1;kernel[0][4]=0;
+	kernel[1][0]=1;kernel[1][1]=1;kernel[1][2]=1;kernel[1][3]=1;kernel[1][4]=1;
+	kernel[2][0]=1;kernel[2][1]=1;kernel[2][2]=1;kernel[2][3]=1;kernel[2][4]=1;
+	kernel[3][0]=1;kernel[3][1]=1;kernel[3][2]=1;kernel[3][3]=1;kernel[3][4]=1;
+	kernel[4][0]=0;kernel[4][1]=1;kernel[4][2]=1;kernel[4][3]=1;kernel[4][4]=0;
+	int scale=273;
+
+	ARUint8 *erodedBuffer=(ARUint8*)malloc(h*w);
+	for (int y=0;y<h;++y) {
+		for (int x=0;x<w;++x) {			
+			size_t pos=640*y+x;
+			if (y<=1 || x<=1 || x>=w-2 || y>=h-2) {
+				erodedBuffer[pos]=data[4*pos+3];
+			}
+			else if (data[4*pos+3]==0) {
+				for (int i=0;i<5;++i) {
+					for (int j=0;j<5;++j) {
+						size_t newPos=640*(y+i-2)+x+j-2;
+						if (kernel[i][j]==1) {
+							erodedBuffer[newPos]=0;
+						}
+					}
+				}
+			}
+		}
+	}
+	for (size_t y=0;y<h;++y) {
+		for (size_t x=0;x<w;++x) {			
+			size_t pos=640*y+x;
+			data[4*pos+3]=erodedBuffer[pos];
+		}
+	}
+	free(erodedBuffer);
+}
+
+void alphaDilate(ARUint8 *data, int w, int h) {
+	int kernel[5][5];
+	kernel[0][0]=0;kernel[0][1]=1;kernel[0][2]=1;kernel[0][3]=1;kernel[0][4]=0;
+	kernel[1][0]=1;kernel[1][1]=1;kernel[1][2]=1;kernel[1][3]=1;kernel[1][4]=1;
+	kernel[2][0]=1;kernel[2][1]=1;kernel[2][2]=1;kernel[2][3]=1;kernel[2][4]=1;
+	kernel[3][0]=1;kernel[3][1]=1;kernel[3][2]=1;kernel[3][3]=1;kernel[3][4]=1;
+	kernel[4][0]=0;kernel[4][1]=1;kernel[4][2]=1;kernel[4][3]=1;kernel[4][4]=0;
+	int scale=273;
+
+	ARUint8 *dilatedBuffer=(ARUint8*)malloc(h*w);
+	for (int y=0;y<h;++y) {
+		for (int x=0;x<w;++x) {			
+			size_t pos=640*y+x;
+			if (y<=1 || x<=1 || x>=w-2 || y>=h-2) {
+				dilatedBuffer[pos]=data[4*pos+3];
+			}
+			else if (data[4*pos+3]==255) {
+				for (int i=0;i<5;++i) {
+					for (int j=0;j<5;++j) {
+						size_t newPos=640*(y+i-2)+x+j-2;
+						if (kernel[i][j]==1) {
+							dilatedBuffer[newPos]=255;
+						}
+					}
+				}
+			}
+		}
+	}
+	for (size_t y=0;y<h;++y) {
+		for (size_t x=0;x<w;++x) {			
+			size_t pos=640*y+x;
+			data[4*pos+3]=dilatedBuffer[pos];
+		}
+	}
+	free(dilatedBuffer);
+}
+
+void alphaHisteresis(ARUint8 *data, int w, int h,int lowThres, int highThres) {
+	// First Pass
+	for (int y=0;y<h;++y) {
+		for (int x=0;x<w;++x) {			
+			if (data[4*(w*y+x)+3]>=highThres) {
+				data[4*(w*y+x)+3]=255;
+				if (x!=w-1 && data[4*(w*y+x+1)+3]>=lowThres) {data[4*(w*y+x+1)+3]=255;}
+				if (y!=h-1 && data[4*(w*(y+1)+x)+3]>=lowThres) {data[4*(w*(y+1)+x)+3]=255;}
+				if (x!=w-1 && y!=h-1 && data[4*(w*(y+1)+x+1)+3]>=lowThres) {data[4*(w*(y+1)+x+1)+3]=255;}
+			}
+		}
+	}
+	// Second Pass
+	for (int y=h-1;y>=0;--y) {
+		for (int x=w-1;x>=0;--x) {			
+			if (data[4*(w*y+x)+3]>=highThres) {
+				data[4*(w*y+x)+3]=255;
+				if (x!=0 && data[4*(w*y+x-1)+3]>=lowThres) {data[4*(w*y+x-1)+3]=255;}
+				if (y!=0 && data[4*(w*(y-1)+x)+3]>=lowThres) {data[4*(w*(y-1)+x)+3]=255;}
+				if (x!=0 && y!=0 && data[4*(w*(y-1)+x-1)+3]>=lowThres) {data[4*(w*(y-1)+x-1)+3]=255;}
+			}
+		}
+	}
+	// Cleanup
+	for (int y=0;y<h;++y) {
+		for (int x=0;x<w;++x) {			
+			if (data[4*(w*y+x)+3]!=255) {
+				data[4*(w*y+x)+3]=0;
+			}
+		}
+	}
+}
+
+void alphaGaussianBlur(ARUint8 *data, int w, int h) {
+	int kernel[5][5];
+	kernel[0][0]=1;kernel[0][1]=4;kernel[0][2]=7;kernel[0][3]=4;kernel[0][4]=1;
+	kernel[1][0]=4;kernel[1][1]=16;kernel[1][2]=26;kernel[1][3]=16;kernel[1][4]=4;
+	kernel[2][0]=7;kernel[2][1]=26;kernel[2][2]=41;kernel[2][3]=26;kernel[2][4]=7;
+	kernel[3][0]=4;kernel[3][1]=16;kernel[3][2]=26;kernel[3][3]=16;kernel[3][4]=4;
+	kernel[4][0]=1;kernel[4][1]=4;kernel[4][2]=7;kernel[4][3]=4;kernel[4][4]=1;
+	int scale=273;
+
+	ARUint8 *smoothBuffer=(ARUint8*)malloc(h*w);
+	for (int y=0;y<h;++y) {
+		for (int x=0;x<w;++x) {			
+			size_t pos=640*y+x;
+			if (y<=1 || x<=1 || x>=w-2 || y>=h-2) {
+				smoothBuffer[pos]=data[4*pos+3];
+			}
+			else {
+				unsigned int accum=0;
+				for (int i=0;i<5;++i) {
+					for (int j=0;j<5;++j) {
+						size_t newPos=640*(y+i-2)+x+j-2;
+						accum+=kernel[i][j]*data[4*(newPos)+3];
+					}
+				}
+				smoothBuffer[pos]=accum/scale;
+			}
+		}
+	}
+	for (size_t y=0;y<h;++y) {
+		for (size_t x=0;x<w;++x) {			
+			size_t pos=640*y+x;
+			data[4*pos+3]=smoothBuffer[pos];
+		}
+	}
+	free(smoothBuffer);
+}
+
 void generateOverMask(ARUint8 *dataIn,ARUint8 *dataOut,int w, int h,int minSat,int maxSat) {
 	for (size_t i=0;i<w*h;i++) {
 		ARUint8 r=dataIn[CHANNELS*i];
@@ -149,41 +293,11 @@ void generateOverMask(ARUint8 *dataIn,ARUint8 *dataOut,int w, int h,int minSat,i
 		}
 	}
 
-	int kernel[5][5];
-	kernel[0][0]=1;kernel[0][1]=4;kernel[0][2]=7;kernel[0][3]=4;kernel[0][4]=1;
-	kernel[1][0]=4;kernel[1][1]=16;kernel[1][2]=26;kernel[1][3]=16;kernel[1][4]=4;
-	kernel[2][0]=7;kernel[2][1]=26;kernel[2][2]=41;kernel[2][3]=26;kernel[2][4]=7;
-	kernel[3][0]=4;kernel[3][1]=16;kernel[3][2]=26;kernel[3][3]=16;kernel[3][4]=4;
-	kernel[4][0]=1;kernel[4][1]=4;kernel[4][2]=7;kernel[4][3]=4;kernel[4][4]=1;
-	int scale=273;
-
-	ARUint8 *smoothBuffer=(ARUint8*)malloc(h*w);
-	for (int y=0;y<h;++y) {
-		for (int x=0;x<w;++x) {			
-			size_t pos=640*y+x;
-			if (y<=1 || x<=1 || x>=w-2 || y>=h-2) {
-				smoothBuffer[pos]=dataOut[4*pos+3];
-			}
-			else {
-				unsigned int accum=0;
-				for (int i=0;i<5;++i) {
-					for (int j=0;j<5;++j) {
-						size_t newPos=640*(y+i-2)+x+j-2;
-						accum+=kernel[i][j]*dataOut[4*(newPos)+3];
-					}
-				}
-				smoothBuffer[pos]=accum/scale;
-			}
-		}
-	}
-	for (size_t y=0;y<h;++y) {
-		for (size_t x=0;x<w;++x) {			
-			size_t pos=640*y+x;
-			dataOut[4*pos+3]=smoothBuffer[pos];
-			//dataOut[4*pos+3]>100?255:dataOut[4*pos+3];
-		}
-	}
-	free(smoothBuffer);
+	alphaGaussianBlur(dataOut,w,h);
+	alphaHisteresis(dataOut,w,h,125,150);
+	//alphaDilate(dataOut,w,h);
+	//alphaErode(dataOut,w,h);
+	alphaGaussianBlur(dataOut,w,h);
 }
 
 /* main loop */
